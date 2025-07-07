cfd62e246831eff6e9d46575f88aa173f06eb4ae
ryan910707 <ryanke91@gmail.com>
2025-07-07 17:21:01 +0800

    add grade.py, add user program

--- user/bigfile.c
+++ user/bigfile.c
@@ -3,17 +3,20 @@
 #include "user/user.h"
 #include "kernel/fcntl.h"
 #include "kernel/fs.h"
+#define fail(msg) do {printf("FAILURE: " msg "\n"); failed = 1; goto done;} while (0);
+static int failed = 0;
 
-int
-main()
+
+static void
+public1()
 {
   char buf[BSIZE];
-  int fd, i, blocks;
+  int fd, blocks;
+  int target = 270;
 
   fd = open("big.file", O_CREATE | O_WRONLY);
   if(fd < 0){
-    printf("bigfile: cannot open big.file for writing\n");
-    exit(-1);
+    fail("bigfile: cannot open big.file for writing\n");
   }
 
   blocks = 0;
@@ -25,34 +28,61 @@ main()
     blocks++;
     if (blocks % 100 == 0)
       printf(".");
+    if(blocks == target)
+      break;
   }
-
   printf("\nwrote %d blocks\n", blocks);
-  if(blocks != 65803) {
-    printf("bigfile: file is too small\n");
-    exit(-1);
+  if(blocks != target) {
+    fail("bigfile: file is too small\n");
   }
-  
+
+  printf("public testcase 1: ok\n");
+
+done:
   close(fd);
-  fd = open("big.file", O_RDONLY);
+  unlink("big.file");
+}
+
+static void 
+public2()
+{
+  char buf[BSIZE];
+  int fd, blocks;
+  int target = 6666;
+
+  fd = open("big.file", O_CREATE | O_WRONLY);
   if(fd < 0){
-    printf("bigfile: cannot re-open big.file for reading\n");
-    exit(-1);
-  }
-  for(i = 0; i < blocks; i++){
-    int cc = read(fd, buf, sizeof(buf));
-    if(cc <= 0){
-      printf("bigfile: read error at block %d\n", i);
-      exit(-1);
-    }
-    if(*(int*)buf != i){
-      printf("bigfile: read the wrong data (%d) for block %d\n",
-             *(int*)buf, i);
-      exit(-1);
-    }
+    fail("bigfile: cannot open big.file for writing\n");
   }
 
-  printf("bigfile done; ok\n"); 
+  blocks = 0;
+  while(1){
+    *(int*)buf = blocks;
+    int cc = write(fd, buf, sizeof(buf));
+    if(cc <= 0)
+      break;
+    blocks++;
+    if (blocks % 100 == 0)
+      printf(".");
+    if(blocks == target)
+      break;
+  }
+  printf("\nwrote %d blocks\n", blocks);
+  if(blocks != target) {
+    fail("bigfile: file is too small\n");
+  }
+  printf("public testcase 2: ok\n");
 
-  exit(0);
+done:
+  close(fd);
+  unlink("big.file");
+}
+
+
+int
+main()
+{
+  public1();
+  public2();
+  exit(failed);
 }
